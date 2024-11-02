78ddfdc1fbbf77f3cb669791f6f90b4f56eb68e8
REZA <rezammdzd0@gmail.com>
2024-11-02 11:00:11 +0330

    feat: fix bug , add interval

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [rows, setRow] = useState(5);
-  const [columns, setColumns] = useState(5);
+  const [rows, setRow] = useState(31);
+  const [columns, setColumns] = useState(31);
   const [world, setWorld] = useState<number[][]>([]);
+  const [intervalId, setIntervalId] = useState<number | null>(null);
 
   useEffect(() => {
     const worldGenerator = (
@@ -27,78 +28,87 @@ function App() {
     setWorld(copy);
   };
 
-  const killerHandler = (rowIndex: number, colIndex: number) => {
-    // debugger;
+  // const killer = (rowIndex: number, colIndex: number) => {
+  //   setWorld((prev) => {
+  //     const copy = JSON.parse(JSON.stringify(prev));
+  //     copy[rowIndex][colIndex] = 0;
+  //     return copy;
+  //   });
+  // };
+  // const reviver = (rowIndex: number, colIndex: number) => {
+  //   setWorld((prev) => {
+  //     const copy = JSON.parse(JSON.stringify(prev));
+  //     copy[rowIndex][colIndex] = 1;
+  //     return copy;
+  //   });
+  // };
 
-    const copy = structuredClone(world);
-    copy[rowIndex][colIndex] = 7;
-    console.log(rowIndex, colIndex);
+  const step = () => {
+    setWorld((prevWorld) => {
+      const copyOfPrevWorld = JSON.parse(JSON.stringify(prevWorld));
+      for (let i = 0; i < prevWorld.length; i++) {
+        for (let j = 0; j < prevWorld[i].length; j++) {
+          const neighbors = {
+            top: prevWorld?.[i - 1]?.[j] || 0,
+            bottom: prevWorld?.[i + 1]?.[j] || 0,
+            right: prevWorld?.[i]?.[j + 1] || 0,
+            left: prevWorld?.[i]?.[j - 1] || 0,
+            topRight: prevWorld?.[i - 1]?.[j + 1] || 0,
+            bottomRight: prevWorld?.[i + 1]?.[j + 1] || 0,
+            topLeft: prevWorld?.[i - 1]?.[j - 1] || 0,
+            bottomLeft: prevWorld?.[i + 1]?.[j - 1] || 0,
+          };
+          const countOfLiveNeighbors = Object.values(neighbors).filter(
+            (i) => i === 1
+          ).length;
 
-    setWorld(copy);
-  };
-
-  const reviverHandler = (rowIndex: number, colIndex: number) => {
-    // const copy = structuredClone(world);
-    // copy[rowIndex][colIndex] = 1;
-
-    setWorld((prev) => {
-      const copy = structuredClone(prev);
-      copy[rowIndex][colIndex] = 1;
-      return copy;
+          if (prevWorld[i][j]) {
+            if (countOfLiveNeighbors <= 1) {
+              // killer(i, j);
+              copyOfPrevWorld[i][j] = 0;
+            }
+            if (countOfLiveNeighbors >= 4) {
+              copyOfPrevWorld[i][j] = 0;
+              // killer(i, j);
+            }
+          } else {
+            if (countOfLiveNeighbors === 3) {
+              // reviver(i, j);
+              copyOfPrevWorld[i][j] = 1;
+            }
+          }
+        }
+      }
+      return copyOfPrevWorld;
     });
   };
 
-  const step = () => {
-    for (let i = 0; i < world.length; i++) {
-      for (let j = 0; j < world[i].length; j++) {
-        // debugger
-        const neighbors = {
-          top: world?.[i - 1]?.[j] || 0,
-          bottom: world?.[i + 1]?.[j] || 0,
-          right: world?.[i]?.[j + 1] || 0,
-          left: world?.[i]?.[j - 1] || 0,
-          topRight: world?.[i - 1]?.[j + 1] || 0,
-          bottomRight: world?.[i + 1]?.[j + 1] || 0,
-          topLeft: world?.[i - 1]?.[j - 1] || 0,
-          bottomLeft: world?.[i + 1]?.[j - 1] || 0,
-        };
-
-        const countOfLiveNeighbors = Object.values(neighbors).filter(
-          (i) => i === 1
-        ).length;
-        // console.log(neighbors, countOfLiveNeighbors);
-
-        // const countOfDeadNeighbors = Object.values(neighbors).filter(
-        //   (i) => i === 0
-        // ).length;
-        if (countOfLiveNeighbors <= 1) {
-          // console.log('in');
-
-          killerHandler(i, j);
-        }
-        // else if (countOfLiveNeighbors >= 4) {
-        //   killerHandler(i, j);
-        // } else if (countOfLiveNeighbors == 3) {
-        //   reviverHandler(i, j);
-        // } else if (countOfLiveNeighbors == 3 || countOfLiveNeighbors == 2) {
-        //   reviverHandler(i, j);
-        // } else {
-        //   throw new Error('ridi');
-        // }
-      }
+  const startHandler = () => {
+    if (!intervalId) {
+      const id = setInterval(() => {
+        step();
+      }, 700);
+      setIntervalId(id);
     }
   };
 
-  console.log(world);
+  const stopHandler = () => {
+    if (intervalId) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+    }
+  };
+
   return (
     <>
       <div className="world">
         {world.map((row, rowIndex) => {
           return (
-            <div className="world__div-row">
+            <div key={rowIndex} className="world__div-row">
               {row.map((r, colIndex) => {
                 return (
                   <div
+                    key={colIndex}
                     className={`world__div-cell ${Boolean(r) ? 'active' : ''}`}
                     onClick={() => onClickHandler(rowIndex, colIndex)}
                   ></div>
@@ -123,7 +133,19 @@ function App() {
         </div>
 
         <div>
-          <button onClick={() => step()}>start</button>
+          <button onClick={() => step()}>one step</button>
+          <button
+            className={`startButton ${intervalId ? 'active' : ''}`}
+            onClick={startHandler}
+          >
+            start
+          </button>
+          <button
+            className={`stopButton ${!intervalId ? 'activeStop' : ''}`}
+            onClick={stopHandler}
+          >
+            stop
+          </button>
         </div>
       </div>
     </>
